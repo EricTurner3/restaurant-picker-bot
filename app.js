3328387aade65d87ae58a3e9641c33d1f7ee788a
Eric Turner <turner.eric7@gmail.com>
2017-01-26 21:15:29 -0500

    Fixed typo

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN); /*?
 
 // URL where the app is running (include protocol). Used to point to scripts and 
 // assets located at this address. 
-const SERVER_URL = (process.env.SERVER_URL); */ ?
+const SERVER_URL = (process.env.SERVER_URL); /* ?
   (process.env.SERVER_URL) :
   config.get('serverURL');*/
 
